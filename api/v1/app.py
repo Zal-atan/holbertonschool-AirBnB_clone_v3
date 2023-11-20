7cb2d46cf3d1162687f26b5da90d936662a2f420
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-19 19:53:02 -0600

    Task13, set up CORS w/ wildcard for any origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# Creating a CORS instance w/ a wildcard for any origin
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
